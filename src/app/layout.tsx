13fbdf735730bde5179e29c5dda1dd16e524bdcf
scholz <moritz.scholz1997@gmail.com>
2024-05-20 13:56:06 +0200

    vsc commmit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
           <div className='flex-1 flex flex-col h-full'>
             <Providers>{children}</Providers>
-          </div>
+        </div>
           <Footer />
         </main>
 
